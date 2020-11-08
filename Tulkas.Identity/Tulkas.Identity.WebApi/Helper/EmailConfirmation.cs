0207926ddde05eba25cb6325182094fd3e42bcde
GÖKHAN İPEK <gokipek91@gmail.com>
2020-11-08 20:44:32 +0300

    commit

--- Tulkas.Identity/Tulkas.Identity.WebApi/Helper/EmailConfirmation.cs
+++ Tulkas.Identity/Tulkas.Identity.WebApi/Helper/EmailConfirmation.cs
@@ -17,7 +17,7 @@ namespace Tulkas.Identity.WebApi.Helper
             mail.Body = $"<h2>Email adresinizi doğrulamak için lütfen aşağıdaki linke tıklayınız.</h2><hr/>";
             mail.Body += $"<a href='{link}'> email doğrulama linki</a>";
             mail.IsBodyHtml = true;
-            smtpClient.Credentials = new NetworkCredential("rackledev@gmail.com", "RackleDev2020?");
+            smtpClient.Credentials = new NetworkCredential("rackledev@gmail.com", "******");
 
             smtpClient.Send(mail);
         }
